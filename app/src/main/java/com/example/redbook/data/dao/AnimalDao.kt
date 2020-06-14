6fdb1579e0dbade0c88786e7c2ed283ac10b609c
Nalog00 <bisenovsaliq@gmail.com>
2020-06-14 17:52:03 +0500

    Detail

--- app/src/main/java/com/example/redbook/data/dao/AnimalDao.kt
+++ app/src/main/java/com/example/redbook/data/dao/AnimalDao.kt
@@ -7,6 +7,9 @@ import com.example.redbook.data.model.Animal
 
 @Dao
 interface AnimalDao {
-    @Query( "SELECT * FROM book")
-    fun getAllAnimals(): List<Animal>
+    @Query( "SELECT * FROM book where type = :type")
+    fun getAllAnimals(type: Int): List<Animal>
+
+    @Query("SELECT * FROM book where id = :id")
+    fun getAnimalById(id: Int): Animal
 }
---
+++ app/src/main/java/com/example/redbook/ui/animal/AnimalListAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.redbook.ui.animal
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.redbook.R
+import com.example.redbook.data.model.Animal
+import kotlinx.android.synthetic.main.item_animal.view.*
+
+class AnimalListAdapter(private val listener: AnimalItemClickListener): RecyclerView.Adapter<AnimalListAdapter.AnimalListViewHolder>() {
+
+    var models: List<Animal> = listOf()
+    set(value) {
+        field = value
+        notifyDataSetChanged()
+    }
+
+    inner class AnimalListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun populateModel(animal: Animal){
+            itemView.tvUzbName.text = animal.nameUzb
+            itemView.tvRusName.text = animal.nameRus
+            itemView.tvEngName.text = animal.nameEng
+            val ImageResName = "picture${animal.id}"
+            Glide.with(itemView)
+                .load(itemView.context.resources.getIdentifier(ImageResName, "drawable", itemView.context.packageName))
+                .into(itemView.ivAnimal)
+
+            itemView.setOnClickListener{
+                listener.onAnimalItemClick(animal.id)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalListViewHolder {
+       val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_animal,parent,false)
+        return AnimalListViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int = models.size
+
+    override fun onBindViewHolder(holder: AnimalListViewHolder, position: Int) {
+       holder.populateModel(models[position])
+    }
+}
